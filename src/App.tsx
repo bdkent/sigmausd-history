bf789c3777bd3f83299eaabff4a34ae4c7004a16
Brian Kent <bdkent@users.noreply.github.com>
2021-05-25 04:39:03 +0000

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -215,8 +215,15 @@ function App(): JSX.Element {
       </div>
 
       <hr />
-
-
+      <footer className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a href="http://sigmausd.io" className="nav-link">sigmausd.io</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
 
     </div>
   );
